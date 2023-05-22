31ac38a4372c89608a8c8d4e89b122278ac09b5b
victorteje <vteje2@gmail.com>
2023-05-22 14:50:47 +0200

    variables done

---
+++ index.js
@@ -0,0 +1,18 @@
+let message = "My age is: ";
+let number = 38;
+let age = message + number;
+
+let math = 1 > 2;
+
+let secondNumber = 10;
+
+
+
+let add = number + secondNumber;
+let substract = number - secondNumber;
+let multiply = number * secondNumber;
+let divide = number / secondNumber;
+
+let secondMessage = "too young to learn JavaScript."
+let actualAge = message + secondMessage;
+
