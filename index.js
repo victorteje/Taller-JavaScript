1802df25b1615b8a95cbef3a4453479f64d0f886
victorteje <vteje2@gmail.com>
2023-05-23 13:37:03 +0200

    reordered logs

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
 let message = "My age is: ";
 let number = 38;
 let age = message + number;
+console.log(age);
+
+let secondMessage = "Too young to learn JavaScript."
+let actualAge = message + secondMessage;
+console.log("On second thought... " + actualAge);
 
 let badMath = 1 > 2;
+console.log("1 > 2 is " + badMath);
 
 let secondNumber = 10;
-
-
-
 let add = number + secondNumber;
 let substract = number - secondNumber;
 let multiply = number * secondNumber;
 let divide = number / secondNumber;
+console.log("38 + 10 equals " + add);
+console.log("38 - 10 equals " + substract);
+console.log("38 x 10 equals " + multiply);
+console.log("38 / 10 equals " + divide);
 
-let secondMessage = "Too young to learn JavaScript."
-let actualAge = message + secondMessage;
-
-console.log(badMath);
-console.log(age);
-console.log(actualAge);
 
 function Greeting (name) {
     return "Hello " + name + ".";
@@ -36,11 +37,11 @@ console.log(Addition(5, 2));
 let evenOrOdd = 6;
 
 if (evenOrOdd % 2 == 0) {
-    console.log("The number is even.");
+    console.log("The number " + evenOrOdd + " is even.");
 }
 
 else {
-    console.log("The number is odd.");
+    console.log("The number " + evenOrOdd + " is odd.");
 }
 
 
