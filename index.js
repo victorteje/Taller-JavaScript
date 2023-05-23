ab2366bfa0a39b77f1a23d9917e519437ee30d87
victorteje <vteje2@gmail.com>
2023-05-23 12:24:35 +0200

    doing functions

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let message = "My age is: ";
 let number = 38;
 let age = message + number;
 
-let math = 1 > 2;
+let badMath = 1 > 2;
 
 let secondNumber = 10;
 
@@ -13,9 +13,22 @@ let substract = number - secondNumber;
 let multiply = number * secondNumber;
 let divide = number / secondNumber;
 
-let secondMessage = "too young to learn JavaScript."
+let secondMessage = "Too young to learn JavaScript."
 let actualAge = message + secondMessage;
 
-console.log(math);
+console.log(badMath);
 console.log(age);
 console.log(actualAge);
+
+function Greeting (name) {
+    return "Hello " + name + ".";
+}
+
+console.log(Greeting("Víctor"));
+
+function Addition (numberOne, numberTwo) {
+    let result = numberOne + numberTwo;
+    return result;
+}
+
+console.log(Addition(5, 2));
