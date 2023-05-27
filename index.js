0c6970c750444c0d0ca1f3f16d4514ff2f570929
victorteje <vteje2@gmail.com>
2023-05-27 09:10:03 +0200

    added document display

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ let message = "My age is: ";
 let number = 38;
 let age = message + number;
 console.log(age);
+document.write(age);
 
 let secondMessage = "Too young to learn JavaScript."
 let actualAge = message + secondMessage;
 console.log("On second thought... " + actualAge);
+document.write("<br>On second thought... " + actualAge);
 
 let badMath = 1 > 2;
 console.log("1 > 2 is " + badMath);
+document.write("<br>1 > 2 is " + badMath);
 
 let secondNumber = 10;
 let add = number + secondNumber;
@@ -16,9 +19,13 @@ let substract = number - secondNumber;
 let multiply = number * secondNumber;
 let divide = number / secondNumber;
 console.log("38 + 10 equals " + add);
+document.write("<br>38 + 10 equals " + add);
 console.log("38 - 10 equals " + substract);
+document.write("<br>38 - 10 equals " + substract);
 console.log("38 x 10 equals " + multiply);
+document.write("<br>38 x 10 equals " + multiply);
 console.log("38 / 10 equals " + divide);
+document.write("<br>38 / 10 equals " + divide);
 
 
 function Greeting (name) {
@@ -26,6 +33,7 @@ function Greeting (name) {
 }
 
 console.log(Greeting("Víctor"));
+document.write(Greeting("<br>Víctor"));
 
 function Addition (numberOne, numberTwo) {
     let result = numberOne + numberTwo;
@@ -33,15 +41,17 @@ function Addition (numberOne, numberTwo) {
 }
 
 console.log(Addition(5, 2));
+document.write("<br>" + Addition(5, 2));
 
-let evenOrOdd = 6;
+let evenOrOdd = 7;
 
 if (evenOrOdd % 2 == 0) {
     console.log("The number " + evenOrOdd + " is even.");
+    document.write("<br>The number " + evenOrOdd + " is even.");
 }
 
 else {
     console.log("The number " + evenOrOdd + " is odd.");
+    document.write("<br>The number " + evenOrOdd + " is odd.");
 }
-
 
